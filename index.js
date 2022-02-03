2d35f3d7be32406481a72d62b2222a64308ab5df
majjikishore007 <majjikishore031@gmail.com>
2022-02-03 19:53:16 +0530

    done with student machine-update

--- index.js
+++ index.js
@@ -4,80 +4,8 @@ import { actions, interpret } from "xstate";
 import { createMachine, assign } from "xstate";
 const app = express();
 
-const enterDetails = assign({
-  details: (context, event) => {
-    console.log("giving test");
-    context.details.name = "majji";
-    context.details.password = "majji";
-  },
-});
-const giveTest = assign({
-  details: (context, event) => {
-    context.details.marks = 70;
-  },
-});
-const check = (context, event) => {
-  return context.marks >= 50;
-};
-const studentMachine = createMachine(
-  {
-    id: "student",
-    initial: "idle",
-    context: {
-      details: {
-        name: "",
-        password: "",
-        marks: 0,
-      },
-    },
-    states: {
-      idle: {
-        on: {
-          KEY_GEN: {
-            target: "enrolmentKeyGenerated",
-            actions: [enterDetails],
-          },
-        },
-      },
-      enrolmentKeyGenerated: {
-        on: {
-          ENTER_DETAILS: {
-            target: "basicDetailsEntered",
-            actions: [giveTest],
-          },
-        },
-      },
-      basicDetailsEntered: {
-        always: {
-          target: "pendingEnglishInterview",
-          cond: check,
-        },
-        on: {
-          INTERVIEW: {
-            target: "testFailed",
-          },
-        },
-      },
-      pendingEnglishInterview: {},
-      testFailed: {
-        on: {
-          INTERVIEW: {
-            target: "pendingEnglishInterview",
-          },
-        },
-      },
-    },
-  },
-  {
-    actions: { enterDetails, giveTest },
-    guards: { check },
-  }
-);
 
-let initialState = "basicDetailsEntered";
-let newstate = studentMachine.transition(initialState, "ENTER_DETAILS");
 
-console.log("initialState: " + newstate.value);
 
 
 
